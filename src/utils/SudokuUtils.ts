5fc40e7ac1c429b5740cd3414a8b17441441e55b
Diego Matricardi <diegobmatricardi@hotmail.com>
2026-09-09 20:39:53 -0300

    Implementação da Funcão de validar movimento.

--- src/utils/SudokuUtils.ts
+++ src/utils/SudokuUtils.ts
@@ -1,9 +1,33 @@
 import { Sudoku } from "../models/sudoku.js";
 
 export class SudokuUtils {
-    static movValido(sudoku: Sudoku, row: number, col: number, num: number):boolean {
-        const tabuleiro = sudoku;
-        //Verificar Linha do tabuleiro
-        return false
+    static movValido(sudoku: Sudoku, linha: number, col: number, num: number):boolean {
+        const tabuleiro = sudoku.getTabuleiro();
+        //Verifica linha do tabuleiro
+        for (let i = 0; i < 9; i++) {
+            if (tabuleiro[linha]![i] === num) {
+                return false;
+            }
+        }
+
+        //Verifica coluna do tabuleiro
+        for (let i = 0; i < 9; i++) {
+            if(tabuleiro[i]![col] === num) {
+                return false;
+            }
+        }
+
+        //Verificar o bloco 3x3 
+        const linhaInicio = Math.floor(linha/3) * 3;
+        const colInicio = Math.floor(col/3) * 3;
+
+        for(let i = linhaInicio; i < linhaInicio + 3; i++) {
+            for(let j = colInicio; j < colInicio + 3; j++) {
+                if(tabuleiro[i]![j] === num){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
